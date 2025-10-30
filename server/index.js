f2d53b695278da70ca035a101660f28fcb279557
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2025-10-30 12:35:22 -0700

    using string concat

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
@@ -10,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get("/api/pages/home", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "/dist/src/pages/home.html"));
+  res.status(200).sendFile(__dirname + "/dist/src/pages/home.html");
 });
 
 app.listen(PORT, () => {
