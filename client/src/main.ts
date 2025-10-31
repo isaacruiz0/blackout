8f39b870dc456a2d7ef295f42face3a192a37d02
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2025-10-31 15:51:03 -0700

    Initializing the single page app according to path

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,29 @@
 import "./style.css";
+
+const getCurrentPage = async (pathname: string) => {
+  let currentPagePromise: Promise<Response> | undefined;
+  const homePagePath = "/src/pages/home.html",
+    careersPagePath = "/src/pages/careers.html",
+    quotePagePath = "/src/pages/quote.html";
+  switch (pathname) {
+    case "/":
+      currentPagePromise = fetch(homePagePath);
+      break;
+    case "/careers":
+      currentPagePromise = fetch(careersPagePath);
+      break;
+    case "/quote":
+      currentPagePromise = fetch(quotePagePath);
+      break;
+  }
+  return currentPagePromise;
+};
+const initPage = async () => {
+  const pathname = window.location.pathname;
+  const currentPageHtml = await getCurrentPage(pathname).then((r) => r?.text());
+  const app = document.getElementById("app");
+  if (!app || !currentPageHtml) return;
+  app.innerHTML = currentPageHtml;
+};
+
+initPage();
