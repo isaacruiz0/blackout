b6abd211d144dc0bf54471934706039d50854135
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2025-11-05 15:37:50 -0800

    using correct types for return

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-// import { Pages } from "./cross_platform_types/pages.ts";
+import type { Pages } from "./cross_platform_types/pages.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,12 +19,9 @@ app.get("/api/all_pages", (req, res: express.Response) => {
 
   try {
     const home = fs.readFileSync(homePath, "utf8"),
-      career = fs.readFileSync(careerPath, "utf8"),
-      quote = fs.readFileSync(quotePath, "utf8");
-    // TODO: fix module import error
-    // const pages = { home, career, quote };
-    // const pages: Pages = { home, career, quote };
-    const pages = { home, career, quote };
+      careers = fs.readFileSync(careerPath, "utf8"),
+      quote = fs.readFileSync(quotePath, "utf8"),
+      pages: Pages = { "/": home, "/careers": careers, "/quote": quote };
     res.status(200).json(pages);
   } catch (err) {
     throw new Error(err);
