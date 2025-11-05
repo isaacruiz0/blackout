f579380fa09eb7aa9708a90d36f90b2b50b96dcf
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2025-11-05 15:39:24 -0800

    careers not career

--- server/cross_platform_types/pages.ts
+++ server/cross_platform_types/pages.ts
@@ -1,5 +1,5 @@
 export type HTML = string;
-const PATHNAMES = ["/", "/career", "/quote"] as const;
+const PATHNAMES = ["/", "/careers", "/quote"] as const;
 export type Pathname = (typeof PATHNAMES)[number];
 export function isValidPathname(value: string): value is Pathname {
   return (PATHNAMES as readonly string[]).includes(value);
